dcae69b3192fc08e7c7c6f33b8b9aea5667f2165
= <cyficowley@gmail.com>
2018-03-10 14:09:16 -0800

    made the server restart when it errors

--- main.py
+++ main.py
@@ -17,38 +17,47 @@ GINSDll = ctypes.cdll.LoadLibrary("giutility.dll")
 
 labels = []
 values = []
-conn = [Qstation.ConnectGIns(0), Qstation.ConnectGIns(1), Qstation.ConnectGIns(3)];
 buffers = []
 global value_map
 value_map = {}
 
-for c in conn:
-	c.init_connection("192.168.1.28")
-	c.read_sample_rate()
-	self=c;
-	for i in range(int(c.read_channel_count())):
-		GINSDll._CD_eGateHighSpeedPort_GetDeviceInfo(self.HCONNECTION.value,self.Channel_InfoName,i,None,self.char)
-		label=self.char.value.decode('UTF-8')
-		if (label!='Timestamp'):
-			labels.append(self.char.value.decode('UTF-8'));\
-	buffers.append(c.yield_buffer())
+def setup():
+	conn = [Qstation.ConnectGIns(0), Qstation.ConnectGIns(1), Qstation.ConnectGIns(3)];
+	for c in conn:
+		c.init_connection("192.168.1.28")
+		c.read_sample_rate()
+		self=c;
+		for i in range(int(c.read_channel_count())):
+			GINSDll._CD_eGateHighSpeedPort_GetDeviceInfo(self.HCONNECTION.value,self.Channel_InfoName,i,None,self.char)
+			label=self.char.value.decode('UTF-8')
+			if (label!='Timestamp'):
+				labels.append(self.char.value.decode('UTF-8'))
+		buffers.append(c.yield_buffer())
 
+setup()
 
 def update_values():
 	values = []
-	for buffer in buffers:
-			data = [];
-			while(len(data) == 0):
-					data = next(buffer)
-			## Get mean over all the rows
-			data = np.array(data)
-			data = np.mean(data, axis=0)
-			data = np.delete(data, 0)
-			for d in data:
-					values.append(d)
-	global value_map
-	value_map = dict(zip(labels, values))
+	try:
+		for buffer in buffers:
+				data = [];
+				while(len(data) == 0):
+						data = next(buffer)
+						print("just got some guud connection")
 
+				## Get mean over all the rows
+				data = np.array(data)
+				data = np.mean(data, axis=0)
+				data = np.delete(data, 0)
+				for d in data:
+						values.append(d)
+		global value_map
+		value_map = dict(zip(labels, values))
+	except StopIteration:
+		print("why are you running htis")
+		buffers.clear()
+		print("REDID IT")
+		setup()
 
 
 
@@ -72,9 +81,8 @@ def activate():
 
 class HostsCrap(Resource):
 	def get(self):
-                global value_map
-                print(value_map)
-                return value_map
+		global value_map
+		return value_map
 
 
 api.add_resource(HostsCrap, '/')
